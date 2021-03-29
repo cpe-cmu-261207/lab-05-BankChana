bff299386021583b4b38e70da8c93d2f05e71a85
BankChana <59277872+BankChana@users.noreply.github.com>
2021-03-29 14:19:10 +0700

    012

--- src/App.js
+++ src/App.js
@@ -1,81 +1,74 @@
-import { createContext, useReducer,useState,useEffect } from "react";
+import { createContext, useEffect, useReducer , useState } from "react";
 import CourseCard from "./components/CourseCard";
 import CourseForm from "./components/CourseForm";
 
-export const TodosContext = createContext({});
+export const CardContext = createContext({});
 
 function App() {
-  const reducer = (state, action) => {
-    switch (action.type) {
-      case "ADD_TODO":
-        return {
-          ...state,
-          myTodos: [...state.myTodos, action.payload],
-        };
-      case "SET_TODO":
-        return {
-          ...state,
-          myTodos: action.payload,
-        };
-      case "DELETE_TODO":
-        return {
-          ...state,
-          myTodos: state.myTodos.filter((todo) => todo.id !== action.payload),
-        };
-      default:
-        throw new Error();
-    }
-  }
-  const [state, dispatch] = useReducer(reducer, {myTodos: [],});
-
-  const [GPA,setGPA] = useState(0.00)
-  const calc = (course) => {
-    var gpa = 0
-    var cred = 0
-    course.forEach(c => {
-      if(c.grade !== "W"){
-        gpa += Number(c.val) *Number(c.cred)
-        cred += Number(c.cred)
-      }      
-    });
-    gpa = Number(gpa)/ Number(cred)
-    if(gpa > 0.00){
-      setGPA(gpa)
-    }else{
-      setGPA(0.00)
-    }
+  const [GPA, setGPA] = useState(0.00);
+  function calculateGPA() {
+    // TODO
+    let grade = 0.00
+    let credit = 0
+    state.currentState.forEach(obj => {
+      if(obj.a !== "W"){
+        grade += Number(obj.g) * Number(obj.c)
+        credit += Number(obj.c)
+      }
+    })
+    grade  = Number(grade) / Number(credit)
+    if(Number(grade) >= 0)
+      setGPA(Number(grade.toPrecision(3)))
+    else setGPA(0.00)
   }
 
-  function fetchTodos() {
-    const localTodos = localStorage.getItem("myTodos");
-    if (localTodos) {
+  const reducer = (state , action) =>{
+    switch(action.type){
+      case "ADD_CARD":
+        return {
+          ...state,
+          currentState: [...state.currentState, action.payload],
+        };
+      case "SET_CARD":
+        return {
+          ...state,
+          currentState: action.payload,
+        };
+      case "DELETE_CARD":
+        return {
+          ...state,
+          currentState: state.currentState.filter((card) => card.id !== action.payload),
+        };
+      }
+  }
+
+  const [state,dispatch] = useReducer(reducer, {currentState:[],})
+  function fetchCards() {
+    const localState = localStorage.getItem("currentState");
+    if (localState) {
       dispatch({
-        type: "SET_TODO",
-        payload: JSON.parse(localTodos),
-      });
+        type: "SET_CARD",
+        payload: JSON.parse(localState),
+      })
     }
+    calculateGPA()
   }
 
-  useEffect(fetchTodos, []);
-
+  useEffect(fetchCards, [])
   useEffect(() => {
-    localStorage.setItem("myTodos", JSON.stringify(state.myTodos));
-    calc(state.myTodos)
-  }, [state.myTodos]); 
+    localStorage.setItem("currentState", JSON.stringify(state.currentState));
+    calculateGPA()
+  }, [state.currentState])
+  
   return (
-    <div>
-      <h1>GPA CALCULATOR</h1>
-      {/* TODO ADD UI */}
-      <TodosContext.Provider value={{ state, dispatch }}>
-      <div className="container mx-auto h-screen">
-      
-        <CourseCard todos={state.myTodos} />
-
-        <CourseForm />
-        <h1>{GPA.toFixed(2)}</h1>
+    <CardContext.Provider value = {{state,dispatch}}>
+      <div>
+        <h1>GPA CALCULATOR</h1>
+        <CourseCard state={state.currentState} />
       </div>
-    </TodosContext.Provider>
-    </div>
+      <CourseForm />
+      <p>{GPA.toFixed(2)}</p>
+    </CardContext.Provider>
   );
 }
 
